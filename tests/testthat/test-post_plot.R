7d98cc4e829d8ad9617d4eddccaf3331c159d38a
yhan <yushi.han@pasteur.fr>
2024-02-07 15:54:02 +0100

    add testthat and write tests for post_plot() and prior_plot()

--- tests/testthat/test-post_plot.R
+++ tests/testthat/test-post_plot.R
@@ -1,3 +1,65 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Test for x.side argument", {
+    # ARGUMENT x.side CAN ONLY BE 1 OR 3
+    expect_error(post_plot(x.side = 5))
+    expect_no_error(post_plot(x.side = 1))
+    expect_no_error(post_plot(x.side = 3))
+    
+    # Test for x.log.scale argument
+    expect_no_error(post_plot(x.log.scale = TRUE))
+    expect_no_error(post_plot(x.log.scale = FALSE))
+    
+    
+    # Test for x.categ argument
+    expect_no_error(post_plot(x.categ = c("A", "B", "C")))
+    
+    
+    # Test for y.side argument
+    expect_error(post_plot(y.side = 5))
+    expect_no_error(post_plot(y.side = 2))
+    expect_no_error(post_plot(y.side = 4))
+    
+    
+    # Test for y.log.scale argument
+    expect_no_error(post_plot(y.log.scale = TRUE))
+    expect_no_error(post_plot(y.log.scale = FALSE))
+    
+    
+    # Test for y.categ argument
+    expect_no_error(post_plot(y.categ = c("A", "B", "C")))
+    
+    
+    # Test for x.color argument
+    expect_no_error(post_plot(x.color = "blue"))
+    
+    
+    # Test for y.color argument
+    expect_no_error(post_plot(y.color = "red"))
+    
+    
+    # Test for main argument
+    expect_no_error(post_plot(main = "Title"))
+    
+    
+    # Tests for combinations of arguments
+    
+    expect_error(post_plot(x.side = 5, x.log.scale = TRUE))
+    
+    
+    # Test for x.categ and x.log.scale arguments combination
+    expect_no_error(post_plot(x.categ = c("A", "B", "C"), x.log.scale = TRUE))
+    
+    # Test for y.side and y.log.scale arguments combination"
+    expect_error(post_plot(y.side = 1, y.log.scale = TRUE))
+    
+    
+    # Test for y.categ and y.log.scale arguments combination
+    expect_no_error(post_plot(y.categ = c("A", "B", "C"), y.log.scale = TRUE))
+    
+    
+    # Test for x.color and y.color arguments combination
+    expect_no_error(post_plot(x.color = "blue", y.color = "red"))
+    
+    
+    # Test for main and x.color arguments combination"
+    expect_no_error(post_plot(main = "Title", x.color = "blue"))
 })
--- tests/testthat/test-prior_plot.R
+++ tests/testthat/test-prior_plot.R
@@ -1,3 +1,118 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("The function prior_plot() works correctly", {
+    expect_no_error(prior_plot(param.reinitial = TRUE))
+    expect_no_error(prior_plot(param.reinitial = FALSE))
+    
+    
+    # Test for xlog.scale argument
+    expect_no_error(prior_plot(xlog.scale = TRUE))
+    expect_no_error(prior_plot(xlog.scale = FALSE))
+    
+    
+    # Test for ylog.scale argument
+    expect_no_error(prior_plot(ylog.scale = TRUE))
+    expect_no_error(prior_plot(ylog.scale = FALSE))
+    
+    
+    # Test for remove.label argument
+    expect_no_error(prior_plot(remove.label = TRUE))
+    expect_no_error(prior_plot(remove.label = FALSE))
+    
+    
+    # Test for remove.x.axis argument
+    expect_no_error(prior_plot(remove.x.axis = TRUE))
+    expect_no_error(prior_plot(remove.x.axis = FALSE))
+    
+    
+    # Test for remove.y.axis argument
+    expect_no_error(prior_plot(remove.y.axis = TRUE))
+    expect_no_error(prior_plot(remove.y.axis = FALSE))
+    
+    
+    # Test for std.x.range argument
+    expect_no_error(prior_plot(std.x.range = TRUE))
+    expect_no_error(prior_plot(std.x.range = FALSE))
+    
+    # Test for std.y.range argument
+    expect_no_error(prior_plot(std.y.range = TRUE))
+    expect_no_error(prior_plot(std.y.range = FALSE))
+    
+    
+    # Test for down.space argument"
+    expect_no_error(prior_plot(down.space = 1))
+    expect_no_error(prior_plot(down.space = 2))
+    
+    
+    # Test for left.space argument"
+    expect_no_error(prior_plot(left.space = 1))
+    expect_no_error(prior_plot(left.space = 2))
+    
+    
+    # Test for up.space argument
+    expect_no_error(prior_plot(up.space = 1))
+    expect_no_error(prior_plot(up.space = 2))
+    
+    
+    # Test for right.space argument
+    expect_no_error(prior_plot(right.space = 1))
+    expect_no_error(prior_plot(right.space = 2))
+    
+    
+    # Test for orient argument"
+    expect_no_error(prior_plot(orient = 1))
+    expect_no_error(prior_plot(orient = 2))
+    
+    # Test for dist.legend argument"
+    expect_no_error(prior_plot(dist.legend = 3.5))
+    expect_no_error(prior_plot(dist.legend = 4.5))
+    
+    
+    # Test for tick.length argument
+    expect_no_error(prior_plot(tick.length = 0.5))
+    expect_no_error(prior_plot(tick.length = 0.6))
+    
+    
+    # Test for box.type argument
+    expect_no_error(prior_plot(box.type = "n"))
+    expect_no_error(prior_plot(box.type = "o"))
+    
+    
+    # Test for amplif.label argument
+    expect_no_error(prior_plot(amplif.label = 1))
+    expect_no_error(prior_plot(amplif.label = 2))
+    
+    # Test for amplif.axis argument
+    expect_no_error(prior_plot(amplif.axis = 1))
+    expect_no_error(prior_plot(amplif.axis = 2))
+    
+    
+    # Test for display.extend argument
+    expect_no_error(prior_plot(display.extend = TRUE))
+    expect_no_error(prior_plot(display.extend = FALSE))
+    
+    # Test for return.par argument
+    expect_no_error(prior_plot(return.par = TRUE))
+    expect_no_error(prior_plot(return.par = FALSE))
+    
+    
+    # Test for all arguments combined
+    expect_no_error(prior_plot(param.reinitial = TRUE,
+                               xlog.scale = TRUE,
+                               ylog.scale = TRUE,
+                               remove.label = FALSE,
+                               remove.x.axis = FALSE,
+                               remove.y.axis = FALSE,
+                               std.x.range = FALSE,
+                               std.y.range = FALSE,
+                               down.space = 2,
+                               left.space = 2,
+                               up.space = 2,
+                               right.space = 2,
+                               orient = 2,
+                               dist.legend = 4.5,
+                               tick.length = 0.6,
+                               box.type = "o",
+                               amplif.label = 2,
+                               amplif.axis = 2,
+                               display.extend = TRUE,
+                               return.par = TRUE))
 })
