9e6d36cbf937e2e6796e3c52de099cd5f05c6c4c
yhan <yushi.han@pasteur.fr>
2024-02-07 15:20:34 +0100

    add testthat and write tests for empty_graph()

--- tests/testthat/test-empty_graph.R
+++ tests/testthat/test-empty_graph.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("empty_graph function argument tests", {
+  
+  # Test for 'text' argument
+  expect_no_error(empty_graph(text = "No Graph"))
+  expect_error(empty_graph(text = NULL))
+  
+  # Test for 'text.size' argument
+  expect_no_error(empty_graph(text.size = 1))
+  expect_error(empty_graph(text.size = NULL))
+  expect_error(empty_graph(text.size = "invalid_size"))
+  
+  # Test for 'title' argument
+  expect_no_error(empty_graph(title = "Graph Title"))
+  expect_error(empty_graph(title = NULL))
+  
+  # Test for 'title.size' argument
+  expect_no_error(empty_graph(title.size = 1.5))
+  expect_error(empty_graph(title.size = NULL))
+  expect_error(empty_graph(title.size = "invalid_size"))
+  
+  
+  # Test with default arguments
+  expect_no_error(empty_graph())
+  
+  # Test with specified arguments
+  expect_no_error(empty_graph(text = "No Graph", text.size = 1, title = "Graph Title", title.size = 1.5))
+  
+  # Test with invalid arguments
+  expect_error(empty_graph(text = NULL, text.size = 1, title = "Graph Title", title.size = 1.5))
+  expect_error(empty_graph(text = "No Graph", text.size = NULL, title = "Graph Title", title.size = 1.5))
+  expect_error(empty_graph(text = "No Graph", text.size = 1, title = NULL, title.size = 1.5))
+  expect_error(empty_graph(text = "No Graph", text.size = 1, title = "Graph Title", title.size = NULL))
+  expect_error(empty_graph(text = "No Graph", text.size = "invalid_size", title = "Graph Title", title.size = 1.5))
+  expect_error(empty_graph(text = "No Graph", text.size = 1, title = "Graph Title", title.size = "invalid_size"))
+  
 })
