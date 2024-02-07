a2b2b90f0cd78069012699f26a138a041f0f0672
yhan <yushi.han@pasteur.fr>
2024-02-07 15:26:51 +0100

    add testthat and write tests for internal_function()

--- tests/testthat/test-internal_function.R
+++ tests/testthat/test-internal_function.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that(".pack_and_function_check function argument tests", {
+    
+    # Test for 'fun' argument
+    expect_error(.pack_and_function_check(fun = NULL, lib.path = NULL, external.function.name = "fun1"))
+    expect_error(.pack_and_function_check(fun = "some_function", lib.path = NULL, external.function.name = "fun1"))
+    
+    # Test for 'lib.path' argument (skipped because already checked in the main function)
+    
+    # Test for 'external.function.name' argument (no specific test)
+    
+    # Test case for all arguments combined
+    
+    # Test with default arguments
+    expect_error(.pack_and_function_check(fun = NULL, lib.path = NULL, external.function.name = "fun1"))
+    
+    # Test with specified arguments
+    expect_error(.pack_and_function_check(fun = "ggplot2::geom_point", lib.path = "C:/Program Files/R/R-4.3.1/library", external.function.name = "fun1"))
+    
 })
